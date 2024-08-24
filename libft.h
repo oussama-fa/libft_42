4ce081746bcbb3abdb1658718e45144015c6e1dc
oussama-fa <caaf4040@gmail.com>
2024-08-24 01:58:35 +0100

    adding

---
+++ libft.h
@@ -0,0 +1,17 @@
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <unistd.h>
+
+
+int     ft_isalnum(int c);
+int     ft_isalpha(int c);
+int     ft_isdigit(int c);
+int     ft_isprint(int c);
+int     ft_isascii(int c);
+size_t  ft_strlen(const char *s);
+int ft_toupper(int c);
+int ft_tolower(int c);
+
+
+#endif
---
+++ main.c
@@ -0,0 +1,10 @@
+#include "libft.h"
+#include <stdio.h>
+
+int main(void)
+{
+	char	s = 'a';
+	printf("%c\n", ft_tolower(s));
+	printf("%c", ft_toupper(s));
+	return 0;
+}
